c5f3f34103ba5c64fd4bb3a56c0df1875e0b59b3
Bhola Kumar <bholakumarmuz67@gmail.com>
2023-03-17 21:51:27 +0530

    first commmit

---
+++ Main.java
@@ -0,0 +1,9 @@
+//import java.util.*;
+class Main{
+    public static void main(String args []){
+        System.out.println("This is my first program  ");
+        
+
+        // this is comment.
+    }
+}
